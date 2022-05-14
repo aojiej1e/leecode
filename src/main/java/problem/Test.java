b8f5fb45af4a97d40a561d1e99eced3dead186e2
aojie <1767127589@qq.com>
2022-05-14 23:56:44 +0800

    Initial commit

---
+++ src/main/java/problem/Test.java
@@ -0,0 +1,12 @@
+package problem;
+
+/**
+ * @author aojie
+ * @Function
+ * @create 2022-05-14 23:56
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
