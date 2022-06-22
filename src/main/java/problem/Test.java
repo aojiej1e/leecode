a6aeb2a6464873733e6ab495467e755084936349
aojie <1767127589@qq.com>
2022-06-22 23:57:00 +0800

    1

--- src/main/java/problem/Test.java
+++ src/main/java/problem/Test.java
@@ -1,5 +1,11 @@
 package problem;
 
+import com.google.common.collect.Comparators;
+import com.google.common.collect.Lists;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * @author aojie
  * @Function
@@ -7,6 +13,29 @@ package problem;
  */
 public class Test {
     public static void main(String[] args) {
+        User user1 = new User();
+        user1.setName("12");
+        user1.setAge(1);
+        User user2 = new User();
+        user2.setName("22");
+        user2.setAge(2);
+        User user3 = new User();
+        user3.setName("12");
+        user3.setAge(11);
+        User user4 = new User();
+        user4.setName("22");
+        user4.setAge(22);
+        ArrayList<User> userList = Lists.newArrayList(user1, user2, user3, user4);
+
+        System.out.println(userList.stream().collect(Collectors.groupingBy(User::getName)));
+
+        NavigableMap<String, List<User>> navigableMap = userList.stream().collect(Collectors.groupingBy(User::getName, TreeMap::new, Collectors.toList()));
+        System.out.println(navigableMap);
+
         System.out.println("Hello World!");
+
+        TreeMap<String, User> treeMap = new TreeMap<>(((o1, o2) -> {
+            return o1.compareTo(o2.substring(5));
+        }));
     }
 }
---
+++ src/main/java/problem/User.java
@@ -0,0 +1,21 @@
+package problem;
+
+import lombok.Data;
+
+/**
+ * @author aojie
+ * @Function
+ * @create 2022-06-22 22:19
+ */
+@Data
+public class User {
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 年龄
+     */
+    private Integer age;
+}
